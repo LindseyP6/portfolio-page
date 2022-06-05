2ff3955e126580b33c2cd96340eb20ca9d148d9e
Lindsey <lindsey.pregent@gmail.com>
2022-06-04 22:59:12 -0400

    resume offcanvas removed

--- client/src/Components/Bio.js
+++ client/src/Components/Bio.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
-import { Document } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+import resume from './resume.pdf';
 
 function Bio() {
   const [show, setShow] = useState(false);
@@ -17,19 +18,19 @@ function Bio() {
           <p>Hi! I'm Lindsey and I am excited to be entering the wide world of coding. After working in customer service and administration for 15 years, I decided on a change. I attended Flatiron School from Jan-April 2022 where I learned HMTL, CSS, JavaScript, React, Ruby and Rails. I'm now a Full Stack Software Engineer! I also work in the cat rescue world as a foster and a TNR (trap-neuter-return) certified trapper. I bring my passion for anti-racism, social justice and cat rescue to everything I do. 
           </p>
           <div className="resume-offcanvas">
-            <Button variant="light" onClick={handleShow}>
-            Resume
+            <Button variant="dark" onClick={handleShow}>
+            <a href="https://www.linkedin.com/in/lindsey-lee-pregent1613/overlay/1635490062046/single-media-viewer/" target="_blank" rel="noreferrer">Resume</a>
           </Button>
 
-      <Offcanvas show={show} onHide={handleClose}>
+      {/* <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>My Resume</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <a href="https://www.linkedin.com/in/lindsey-lee-pregent1613/overlay/1635490062046/single-media-viewer/" target="_blank" rel="noreferrer">Resume</a>
-          <Document file="https://storage.cloud.google.com/all-cats/Lindsey%20L%20Pregent%20SE%20Resume.pdf"></Document>
+          <img src="https://storage.cloud.google.com/all-cats/Screen%20Shot%202022-06-04%20at%2010.50.56%20PM.png" alt="resume-screenshot"/>
         </Offcanvas.Body>
-      </Offcanvas>
+      </Offcanvas> */}
           </div>
         </div>
         <div className="bio-right">
