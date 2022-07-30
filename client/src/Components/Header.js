0f4d62d444b1394e0ee299b2742ecdeb79377ad1
Lindsey <lindsey.pregent@gmail.com>
2022-07-30 16:53:04 -0400

    nav bar changes

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import { HashLink as Link } from "react-router-hash-link";
+import React from "react";
 
-import { HiOutlineMailOpen } from "react-icons/hi";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { VscGithub } from "react-icons/vsc";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { BsMedium } from "react-icons/bs";
 import { AiFillMediumCircle } from "react-icons/ai";
-import { RiTwitterLine } from "react-icons/ri";
-import { BiArrowToTop } from "react-icons/bi";
 
-import Row from "react-bootstrap/Row";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 function Header() {
   return (
-    <div id="header">
-        <Switch>
-            <div className="nav-links">
-              {/* <Link smooth to="/#header">Home</Link> */}
-              <Link smooth to="/#bio">Bio</Link>
-              {/* <Link smooth to='/#tech'>Tech-Stack</Link> */}
-              <Link smooth to='/#projects'>Projects</Link>
-              <Link smooth to="/#footer">Contact</Link>
-              {/* <Link smooth to='/#cats'>Cats</Link> */}
-            </div>
-        </Switch>
-        <div className="my-links">
-          <a href="https://www.linkedin.com/in/lindsey-lee-pregent1613/" target="_blank" rel="noreferrer"><AiOutlineLinkedin /></a>  
-          <a href="https://github.com/LindseyP6" target="_blank" rel="noreferrer"><VscGithub /></a> 
-          <a href="https://medium.com/@lindsey.lee6" target="_blank" rel="noreferrer"><AiFillMediumCircle /></a>
-        </div> 
-    </div>
-  )
+    // <div id="header">
+
+    <Navbar expand="lg" id="header" sticky="top">
+      <Container>
+        <Navbar.Brand
+          href="https://www.linkedin.com/in/lindsey-lee-pregent1613/"
+          target="_blank"
+          rel="noreferrer"
+          bsPrefix="my-links"
+        >
+          <AiOutlineLinkedin />
+        </Navbar.Brand>
+        <Navbar.Brand
+          href="https://github.com/LindseyP6"
+          target="_blank"
+          rel="noreferrer"
+          bsPrefix="my-links"
+
+        >
+          <VscGithub />
+        </Navbar.Brand>
+        <Navbar.Brand
+          href="https://medium.com/@lindsey.lee6"
+          target="_blank"
+          rel="noreferrer"
+                    bsPrefix="my-links"
+
+        >
+          <AiFillMediumCircle />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="nav-links" navbarScroll>
+            <Nav.Link href="#bio">Bio</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#footer">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    //  </div>
+  );
 }
 
-export default Header
+export default Header;
